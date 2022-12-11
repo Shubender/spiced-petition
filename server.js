669802cc4429854a20a26f3fc9e051b9bd23d57b
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2022-12-11 21:18:38 +0100

    sunday stop

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // require modules that you will need and set up your express app (EASY)
 const express = require("express");
 const app = express();
-
 const { getAllSignatures, addSignature } = require("./db.js");
+let showWarning = false;
 
 // setup handlebars for your express app correctly (EASY)
 const { engine } = require("express-handlebars");
@@ -16,8 +16,20 @@ app.use(express.static("./public"));
 app.use(express.static("./public/images"));
 app.use(express.urlencoded());
 
+const cookieParser = require("cookie-parser");
+const req = require("express/lib/request.js");
+app.use(cookieParser());
+app.use((req, res, next) => {
+    if (req.url.startsWith("/petition") && req.cookies.dataOk === "yes") {
+        res.redirect("/thanks");
+    } else {
+        next();
+    }
+});
+
 // Create multiple routes for your express app:
 app.get("/", (req, res) => {
+    showWarning = false;
     res.redirect("/petition/");
 });
 
@@ -25,6 +37,7 @@ app.get("/", (req, res) => {
 app.get("/petition", (req, res) => {
     res.render("petition", {
         layout: "main",
+        showWarning,
     });
 });
 
@@ -32,9 +45,19 @@ app.post("/petition", (req, res) => {
     let fName = req.body.fname;
     let lName = req.body.lname;
     const userSignature = "test";
+
+    if (fName === "" || lName === "") {
+        showWarning = true;
+        res.redirect("/petition/");
+    }
+
+    if (fName !== "" && lName !== "") {
+        showWarning = false;
+        res.cookie("dataOk", "yes");
+        addSignature(fName, lName, userSignature);
+        res.redirect("/thanks/");
+    }
     // console.log("First name: ", fName, "Last name: ", lName);
-    addSignature(fName, lName, userSignature);
-    res.redirect("/thanks/");
 });
 
 let signersCount;
@@ -61,14 +84,17 @@ app.get("/thanks", (req, res) => {
 });
 
 //  - one route for rendering the signers page with handlebars (EASY); make sure to get all the signature data from the db before (MEDIUM)
-app.get("/signers", (req, res) => {
-    res.render("signers", {
-        layout: "main",
-        signersCount,
-        allData,
-    });
-});
-
 //  - one route for POSTing petition data -> update db accordingly (MEDIUM)
+app.get("/signers", (req, res) => {
+    getAllSignatures()
+        .then((data) => {
+            allData = data.rows;
+            res.render("signers", {
+                layout: "main",
+                allData,
+            });
+        })
+        .catch((err) => console.log(err));
+});
 
 app.listen(8080, console.log("running at 8080"));
