e355b375b83307777c30f66f7214caffe95ed1db
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2022-12-14 15:48:22 +0100

    change db.js

--- db.js
+++ db.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
-const { SQL_USER, SQL_PASSWORD } = process.env; // add a .env file next to the db.js file with your PostgreSQL credentials
+const { DATABASE_URL } = process.env; // add a .env file next to the db.js file with your PostgreSQL credentials
 const spicedPg = require("spiced-pg");
-const db = spicedPg(
-    `postgres:${SQL_USER}:${SQL_PASSWORD}@localhost:5432/petition`
-);
+const db = spicedPg(DATABASE_URL);
 
 // create the following functions:
 //  - getAllSignatures - use db.query to get all signatures from table signatures
